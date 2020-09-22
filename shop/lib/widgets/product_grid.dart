de418c60af24b53c9c4d7e3be99482daf2467a13
Fernanda Tachibana <fernanda.tachibana@ecommit.com.br>
2020-09-22 14:21:21 -0300

    Final Commit

---
+++ shop/lib/widgets/product_grid.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/product.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  final bool showFavoriteOnly;
+
+  ProductGrid(this.showFavoriteOnly);
+
+  @override
+  Widget build(BuildContext context) {
+    final loadedProducts = Provider.of<Products>(context);
+    final products = showFavoriteOnly ? loadedProducts.favoriteItems : loadedProducts.items;
+    return GridView.builder(
+      padding: EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
+        child: ProductItem(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
